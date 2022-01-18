4745844c139df48cfaee92b46492a2190ff675e1
wwwalohacampus <h850415@naver.com>
2022-01-18 15:53:53 +0900

    4일차

---
+++ Day04/ex02.py
@@ -0,0 +1,11 @@
+name = '홍길동'
+print('내 이름은 %s 입니다' % name)
+
+height = 160.25
+print('내 키는 %.2f cm 입니다' % height)
+
+weight = 40.5
+print('내 몸무게는 %.2f kg 입니다' % weight)
+
+year,month,day = 2022,1,18
+print('내 생일은 %d년 %d월 %d일 입니다.' %(year,month,day))
